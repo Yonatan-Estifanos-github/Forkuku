d054e76eff1f2d141f79eb114ad6b32bc39bbc7b
Yonatan Estifanos <yonatanestifanos58850@gmail.com>
2026-03-26 00:33:51 -0400

    fix: remove footer tagline from SaveTheDate email

--- src/emails/SaveTheDate.tsx
+++ src/emails/SaveTheDate.tsx
@@ -42,9 +42,7 @@ export const SaveTheDate = ({
           </Button>
         </Section>
         
-        <Text style={footer}>
-          Made with ♥ by The Estifanos Family
-        </Text>
+
       </Container>
     </Body>
   </Html>
